e92d935f6e51014262835464d81a7203ad780fe6
mazharulsabbir <mazharul15-8950@diu.edu.bd>
2020-10-02 21:19:27 +0600

    encrypted password

--- src/main/java/com/mazharulsabbir/restapispring/MyUserDetailService.kt
+++ src/main/java/com/mazharulsabbir/restapispring/MyUserDetailService.kt
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service
 @Service
 object MyUserDetailService : UserDetailsService {
     override fun loadUserByUsername(username: String?): UserDetails {
-        return User("admin", "admin", mutableListOf())
+        val password = "\$2y\$10\$6QW1ATLIGw.VQ6n/YdmxT.bjEYGOVm7UD9Z08aR/UXVHTVH6Y26bi" // encrypt of admin using BCrypt
+        return User("admin", password, mutableListOf())
     }
 }
--- src/main/java/com/mazharulsabbir/restapispring/WebSecurityConfig.java
+++ src/main/java/com/mazharulsabbir/restapispring/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -27,7 +28,7 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     @Override
