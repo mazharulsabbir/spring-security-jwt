4a4eafb22cd50be46fa3ec06f23a602346eba1a4
sabbir <mazharul15-8950@diu.edu.bd>
2020-10-03 09:13:55 +0600

    mysql setup

--- src/main/java/com/mazharulsabbir/restapispring/data/user/User.kt
+++ src/main/java/com/mazharulsabbir/restapispring/data/user/User.kt
@@ -1,9 +1,25 @@
 package com.mazharulsabbir.restapispring.data.user
 
+import java.util.*
+import javax.persistence.Entity
+import javax.persistence.Id
+
+@Entity
 data class User(
+        @Id
+        val uid: UUID,
         val username: String? = null,
         val email: String? = null,
         val blood_group: String? = null,
         val mobile: String? = null,
         val address: String? = null
-)
+) {
+    constructor() : this(
+            UUID.randomUUID(),
+            null,
+            null,
+            null,
+            null,
+            null
+    )
+}
